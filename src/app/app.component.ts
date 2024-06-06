10b5cf88e9d260cd0801a0327d5dee28342230ac
Anett <kovacs.anett91@gmail.com>
2024-06-06 13:07:46 +0200

    The required changes are done.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,21 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common'; 
 import { NgxMasonryModule } from 'ngx-masonry';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'kid-art-wall';
+  
+  images = [
+    { src: '/assets/img/christmas.jpg', alt: 'Image 1' },
+    { src: '/assets/img/easter_bunnies.jpg', alt: 'Image 2' },
+    { src: '/assets/img/spring_bird.jpg', alt: 'Image 3' }
+  ];
 }
